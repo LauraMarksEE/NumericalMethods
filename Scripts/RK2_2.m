f8116da0179abf9c56aee6e4376d0ebfe9a6d5d2
Laura <writetomool@gmail.com>
2020-02-21 16:28:18 +0000

    More things

--- Scripts/RK2_2.m
+++ Scripts/RK2_2.m
@@ -1,17 +1,17 @@
 ti = 0; qi = 500*10^-9;
-h = 0.000001; tf = 1*10^-3;
+h = 0.00011; tf = 7*10^-2;
 
 R = 1000;
 C = 100*10^-9;
 tau = 100*10^-6;
-tp = 100*10^-6; %Vin time period
+tp = 1000*10^-6; %Vin time period
 
 % Vin = @(t) 2.5;
-% Vin = @(t) 2.5*exp(-(t^2)/tau); %exp
+Vin = @(t) 2.5*exp(-(t.^2)/tau); %exp
 % Vin = @(t) 5*sin((2*pi*t)/tp); %sine
 % Vin = @(t) 5*sign(cos((2*pi*t)/tp)); %square
 % Vin = @(t) 10*(2*floor(t/tp)-floor(2*t/tp))+5; %square
-Vin = @(t) 5*2*((t/tp)-floor(0.5+(t/tp))); %sawtooth
+% Vin = @(t) 5*2*((t/tp)-floor(0.5+(t/tp))); %sawtooth
 
 func = @(t, q) (1/R)*(Vin(t) - (1/C)*q);
 
@@ -35,12 +35,13 @@ hold on;
 plot(tt,qq/C, 'r-');
 hold on;
 
-% plot(tt, qSol(tt)/C, 'r-.');
+plot(tt, qSol(tt)/C, 'r-.');
 
-title('Vin');
+title('V_I = 5sin(2{\pi}*T=1000{\mu}s) V');
 xlabel('Time/s')
 ylabel('V_o/V')
 legend('Vin', 'Runge-Kutta Method', 'Exact Values');
 
 %print -depsc VinH7
+
 % --------------------------------------------
